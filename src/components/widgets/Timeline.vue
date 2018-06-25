a31e775a948aafd4d65faf2b31269cae03484325
Alexandre9118 <alexandre.combemorel@homeday.de>
2018-06-25 12:44:14 +1000

    animation part1

---
+++ src/components/widgets/Timeline.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="timeline">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Timeline',
+  data() {
+    return {
+      msg: 'Hello world',
+    };
+  },
+};
+</script>
